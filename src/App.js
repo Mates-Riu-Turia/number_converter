b5d10f3d6f3beda1b534bd359ba4facc76e924cf
Juanjo Asensio García <asengar2009@gmail.com>
2023-07-01 01:05:09 +0200

    Create a Nav menu

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import {React, useState} from "react";
+import { useTranslation } from "react-i18next";
+
+function Nav({ t, changeLanguage }) {
+    const [state, setState] = useState(" " + t("chooseLang"));
+
+    function changeLanguageCa() {
+        changeLanguage("ca");
+        setState(" CA");
+    }
+
+    function changeLanguageEs() {
+        changeLanguage("es");
+        setState(" ES");
+    }
+
+    function changeLanguageEn() {
+        changeLanguage("en");
+        setState(" EN")
+    }
+
+    return (
+        <nav className="navbar bg-body-tertiary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">
+                    <img src="favicon.ico" alt="Logo" width="30" height="24" className="d-inline-block align-text-top me-1" />
+                    {t("title")}
+                </a>
+                <div className="dropdown me-4">
+                    <button className="btn btn-secondary dropdown-toggle rounded-pill" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i className="bi bi-globe-americas"></i><span id="langSelector">{state}</span>
+                    </button>
+                    <ul className="dropdown-menu dropdown-menu-end">
+                        <li><button className="dropdown-item" type="button" onClick={changeLanguageCa}>Valencià/Català</button></li>
+                        <li><button className="dropdown-item" type="button" onClick={changeLanguageEs}>Castellano</button></li>
+                        <li><button className="dropdown-item" type="button" onClick={changeLanguageEn}>English</button></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default function App() {
+    const { t, i18n } = useTranslation();
+
+    const changeLanguage = (lng) => {
+        i18n.changeLanguage(lng);
+    };
+
+    // Set the Web Page Title
+    document.title = t("title");
+
+    return (
+        <>
+            <Nav t={t} changeLanguage={changeLanguage} />
+        </>
+    );
+}
